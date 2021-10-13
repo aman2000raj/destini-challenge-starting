f584b5310d84459ba53c839332330e79045f42d8
gupta-apurva <38276094+gupta-apurva@users.noreply.github.com>
2021-10-13 12:18:50 +0530

    complete

--- android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
+++ android/app/src/main/java/co/appbrewery/destinichallengestarting/MainActivity.java
@@ -1,8 +1,7 @@
 package co.appbrewery.destinichallengestarting;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+ 
 }
